ba62fef810563f5b0444afd6e86164bdd6388b7e
EduardoSilva7 <128163191+EduardoSilva7@users.noreply.github.com>
2025-10-07 17:45:58 -0300

    Delete gamehub directory

--- gamehub/src/main/java/com/gamehub/app/repository/GameRepository.java
+++
@@ -1,6 +0,0 @@
-package com.gamehub.app.repository;
-
-import com.gamehub.app.model.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameRepository extends JpaRepository<Game, Long> { }
